4f6200a37d1181e295b245c7f1dc6cb101b2b798
Akbar <m.akbar97@gmail.com>
2023-07-19 18:17:38 +0500

    sixth commit

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ def addition():
             break
         except ValueError:
             print("Invalid input! Please enter valid numbers.")
-            print("merging")
+            print("merging 2")
 
 
 addition()
