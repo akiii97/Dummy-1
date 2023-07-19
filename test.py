d13da8efcbf6c3dc8c779a402c257f138d9dcf0f
Akbar <m.akbar97@gmail.com>
2023-07-19 15:52:25 +0500

    second commit

--- test.py
+++ test.py
@@ -1,2 +1,43 @@
-print("Hello World")
-print("for pull")
+def addition():
+    print("Enter numbers for Addition")
+    num1 = float(input('Enter First Number: '))
+    num2 = float(input('Enter Second Number: '))
+    result = num1 + num2
+    print("The answer to added value is", result)
+
+
+addition()
+
+
+def subtraction():
+    print("Enter numbers for Subtraction")
+    num1 = float(input('Enter First Number: '))
+    num2 = float(input('Enter Second Number: '))
+    result = num1 - num2
+    print("The answer to subtracted value is", result)
+
+
+subtraction()
+
+
+def division():
+    print("Enter numbers for Division")
+    num1 = float(input('Enter First Number: '))
+    num2 = float(input('Enter Second Number: '))
+    result = num1 / num2
+    print("The answer to divided value is", result)
+
+
+division()
+
+
+def multiplication():
+    print("Enter numbers for Multiplication")
+    num1 = float(input('Enter First Number: '))
+    num2 = float(input('Enter Second Number: '))
+    result = num1 * num2
+    print("The answer to multiplied value is", result)
+
+
+multiplication()
+
